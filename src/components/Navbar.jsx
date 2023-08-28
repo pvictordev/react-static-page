e27b6081d531ba44a9041c510da64e17bf5e047a
Purice Victor <pvictor0110@gmail.com>
2023-08-28 15:41:24 +0300

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,10 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img src={"./src/img/react-icon-small.png"} className="nav--icon" />
-            <h3 className="nav--logo_text">ReactFacts</h3>
+            <div className="nav--logo">
+                <img src={"./src/img/react-icon-small.png"} className="nav--icon" />
+                <h3 className="nav--logo_text">ReactFacts</h3>
+            </div>
             <h4 className="nav--title">React Static Page</h4>
         </nav>
     )
