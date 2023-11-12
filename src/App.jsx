57ea1a3b03a7e7598418c80b6edf29cd637ed058
Purice Victor <pvictor0110@gmail.com>
2023-11-12 19:55:49 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import Main from "./components/Main"
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 export default function App() {
-    return (
-        <>
-          <Navbar />
-          <Main />
-        </>
-    )
+  const [mode, setMode] = useState(false);
+
+  const toggleMode = () => {
+    setMode((prev) => !prev);
+  };
+  useEffect(() => {
+    if (mode) {
+      document.body.style.backgroundColor = "#deebf8";
+    } else {
+      document.body.style.backgroundColor = "#21222a";
+    }
+  });
+
+  return (
+    <>
+      <Navbar toggleMode={toggleMode} mode={mode} />
+      <Main mode={mode} />
+    </>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,18 @@
-import React from "react"
+import React, { useState } from "react";
+import { BsToggleOff, BsToggleOn } from "react-icons/bs";
 
-export default function Navbar() {
-    return (
-        <nav>
-            <div className="nav--logo">
-                <img src={"./src/img/react-icon-small.png"} className="nav--icon" />
-                <h3 className="nav--logo_text">ReactFacts</h3>
-            </div>
-            <h4 className="nav--title">React Static Page</h4>
-        </nav>
-    )
+export default function Navbar({ toggleMode, mode }) {
+  return (
+    <nav className={`${mode ? "nav light-nav" : "nav"}`}>
+      <div className="nav--logo">
+        <img src={"./src/img/react-icon-small.png"} className="nav--icon" />
+        <h3 className="nav--logo_text">ReactFacts</h3>
+      </div>
+      {mode ? (
+        <BsToggleOff style={{ fontSize: "40px" }} onClick={toggleMode} />
+      ) : (
+        <BsToggleOn style={{ fontSize: "40px" }} onClick={toggleMode} />
+      )}
+    </nav>
+  );
 }
